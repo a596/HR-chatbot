4739ee40e267a6c8dd05e41060c685489cdfd28f
bc2000414844 <155548970+bc2000414844@users.noreply.github.com>
2024-09-10 15:59:03 +0500

    Your commit message

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,7 @@
-name = name
+
+from nltk.tag import pos_tag
+from nltk.tokenize import word_tokenize
+from nltk.chunk import ne_chunk
+from flask_mail import Mail, Message
+from groq import Groq
+load_dotenv()
